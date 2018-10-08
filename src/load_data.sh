78f5f1072b6b8928be60385edfcf7e57d5bf86a4
hugrave <edoardo.conte@live.it>
2018-10-08 18:54:53 +0200

    Added scripts

---
+++ src/load_data.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Through the current script it will be possible to generate data
+# from tpcds tool and load it to the data warehouse through SparkSQL
+
+print_line() {
+  echo "#############################"
+}
+
+fatal() {
+  print_line
+  echo "$1"
+  print_line
+  exit 0
+}
+
+verify_result() {
+  if [ $? -ne 1 ]; then
+    fatal $1
+  fi
+}
+
+check_env() {
+  if [ -z "$SPARK_HOME" ]; then
+    fatal "SPARK_HOME env variable needs to be set"
+  fi
+
+  GEN_DATA=${GEN_DATA:="true"}
+
+  if [ -z "$DATA_DIR" ]; then
+    fatal "Directory for the data needs to be set"
+  fi
+}
+
+
+# Set all the environment variables
+. ./set_env.sh
+check_env
+
+if [ "$GEN_DATA" = "true" ]; then
+  $TPCDS_ROOT_DIR/tools/dsdgen -SCALE 1 -DIR $DATA_DIR -FORCE -VERBOSE
+  verify_result "Generation of dataset has failed"
+fi
+
+# Evaluating the ddl scripts
+# according to the actual env variables
+DDL=$(pwd)/../ddl
+while read line; do
+  eval "echo \"$line\"";
+done < $DDL/create_database_template.sql > $DDL/create_database.sql
+
+while read line; do
+  line=${line//\"/\\\"}
+  eval "echo \"$line\"";
+done < $DDL/create_tables_template.sql > $DDL/create_tables.sql
+
+
+# Executing the command in SparkSQL
+current_dir=$(pwd)
+cd $SPARK_HOME
+bin/spark-sql --conf spark.sql.catalogImplementation=hive -f ${DDL}/create_database.sql > ${DDL}/create_database.out
+# verify_result "Error in creating the database"
+
+bin/spark-sql --conf spark.sql.catalogImplementation=hive -f ${DDL}/create_tables.sql > ${DDL}/create_tables.out
+# verify_result "Error in loading the tables"
+
+bin/spark-sql --conf spark.conf.catalogImplementation=hive -f ${DDL}/../query/select.sql
---
+++ src/set_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Spark home directory
+export SPARK_HOME=/Users/edoardo/Documents/BDMA/Data_Warehouses/Project/spark
+
+# Either true or false
+export GEN_DATA=false
+
+# Dataset location
+export DATA_DIR=/Users/edoardo/Documents/BDMA/Data_Warehouses/Project/dataSet
+
+# Direcotry for the TPC-DS tool
+export TPCDS_ROOT_DIR=../tpcds
+
+# Database information
+export DB_NAME=TPCDS_DB
